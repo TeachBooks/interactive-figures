25334c2a7813e49eb07dbaa56038a547b1143606
Huffwut <r.zubavicius@tudelft.nl>
2025-08-13 16:02:36 +0200

    finalized bivariate gaussian example

--- src/main2.js
+++ src/main2.js
@@ -13,55 +13,70 @@ import { Gaussian } from "./interactions/Gaussian.js";
   const mapper = new ElementMapper(template.svgElement);
   mapper.buildMap();
 
+  const meanText = mapper.get('text1222').textContent;
+  const sigmaXText = mapper.get('text1222-2').textContent;
+  const sigmaYText = mapper.get('text1222-7').textContent;
 
   const plotter = mapper.preparePlotter('xy-1', [-6, 6], [-6, 6], true);
   const xy_scatter = mapper.preparePlotter('xy-2', [-6, 6], [-6, 6], true);
 
   // Create Gaussian
-  const g = new Gaussian([0, 0], [[1, 0], [0, 1]]);
-  scatterPlot(xy_scatter, g.sample(100), 0.5, "blue", true);
+  const g = new Gaussian([0, 0], 0.6, 1, 1);
+
+  const samples = g.sample(1000);
+  scatterPlot(xy_scatter, samples, 0.5, "blue", true);
 
   // Draw contours
   g.drawContours(plotter, [1, 2, 3]);
 
-  const slider1 = document.getElementById("meanX1");
-  slider1.addEventListener("input", (e) => {
-    const a = parseFloat(e.target.value); // new variance for x-axis
-    const [[var1, cov1], [cov2, var2]] = g.cov; // current values
-    g.update([a, g.mu[1]], [[var1, cov1], [cov2, var2]]);
+  const sliderMeanX1 = document.getElementById("meanX1");
+  sliderMeanX1.addEventListener("input", (e) => {
+    const a = parseFloat(e.target.value); // new variance
+    g.update([a, g.mu[1]], g.sigma_x, g.sigma_y, g.corr);
+    mapper.get('text1222').textContent = meanText + ` (${g.mu[0]}, ${g.mu[1]})`;
     g.drawContours(plotter, [1, 2, 3]);
-    scatterPlot(xy_scatter, g.sample(100), 0.5, "blue", true);
+    scatterPlot(xy_scatter, g.transformSamples(samples), 0.5, "blue", true);
   });
 
-  const slider2 = document.getElementById("var1");
-  slider2.addEventListener("input", (e) => {
+  const sliderMeanX2 = document.getElementById("meanX2");
+  sliderMeanX2.addEventListener("input", (e) => {
+    const a = parseFloat(e.target.value); // new variance
+    g.update([g.mu[0], a], g.sigma_x, g.sigma_y, g.corr);
+    mapper.get('text1222').textContent = meanText + ` (${g.mu[0]}, ${g.mu[1]})`;
+    g.drawContours(plotter, [1, 2, 3]);
+    scatterPlot(xy_scatter, g.transformSamples(samples), 0.5, "blue", true);
+  });
+
+  const sliderStdX1 = document.getElementById("stdX1");
+  sliderStdX1.addEventListener("input", (e) => {
     const a = parseFloat(e.target.value); // new variance for y-axis
-    console.log("Variance slider2:", a);
-    const [[var1, cov1], [cov2, var2]] = g.cov; // current values
-    g.update(g.mu, [[a, cov1], [cov2, var2]]);
+    g.update(g.mu, a, g.sigma_y, g.corr);
+    mapper.get('text1222-7').textContent = sigmaXText + ` ${a}`;
+    mapper.get('loc-00').textContent = a;
     g.drawContours(plotter, [1, 2, 3]);
-    scatterPlot(xy_scatter, g.sample(100), 0.5, "blue", true);
+    scatterPlot(xy_scatter, g.transformSamples(samples), 0.5, "blue", true);
   });
 
-  const slider3 = document.getElementById("var2");
-  slider3.addEventListener("input", (e) => {
-    const a = parseFloat(e.target.value); // new covariance
-    console.log("Covariance slider3:", a);
-    const [[var1, cov1], [cov2, var2]] = g.cov; // current values
-    const [mx, my] = g.mu; // current means
-    g.update([mx, my], [[var1, cov1], [cov2, a]]);
+  const sliderStdX2 = document.getElementById("stdX2");
+  sliderStdX2.addEventListener("input", (e) => {
+    const a = parseFloat(e.target.value); // new variance for y-axis
+    mapper.get('text1222-2').textContent = sigmaYText + ` ${a}`;
+    g.update(g.mu, g.sigma_x, a, g.corr);
+    mapper.get('loc-11').textContent = a;
     g.drawContours(plotter, [1, 2, 3]);
-    scatterPlot(xy_scatter, g.sample(100), 0.5, "blue", true);
+    scatterPlot(xy_scatter, g.transformSamples(samples), 0.5, "blue", true);
   });
 
-  const slider4 = document.getElementById("stdX2");
-  slider4.addEventListener("input", (e) => {
-    const a = parseFloat(e.target.value); // new covariance
-    console.log("Covariance slider4:", a);
-    const [[var1, cov1], [cov2, var2]] = g.cov; // current values
-    const [mx, my] = g.mu; // current means
-    g.update([mx, my], [[var1, a], [a, var2]]);
+  const corrSlider = document.getElementById("Correlation");
+  corrSlider.addEventListener("input", (e) => {
+    const a = parseFloat(e.target.value); // new correlation
+    g.update(g.mu, g.sigma_x, g.sigma_y, a);
+    const [[sigmax,sigma_xy], [sigma_yx,sigmay]] = g.cov;
+    mapper.get('loc-01').textContent = sigma_xy.toFixed(2);
+    mapper.get('loc-10').textContent = sigma_yx.toFixed(2);
+    mapper.get('loc-00').textContent = sigmax.toFixed(2);
+    mapper.get('loc-11').textContent = sigmay.toFixed(2);
     g.drawContours(plotter, [1, 2, 3]);
-    scatterPlot(xy_scatter, g.sample(100), 0.5, "blue", true);
+    scatterPlot(xy_scatter, g.transformSamples(samples), 0.5, "blue", true);
   });
 })();
